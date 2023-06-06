5cc2adca8389749c8e475ad481ab3b67a6d96d91
Peter Christiansen <repetepc@gmail.com>
2023-06-07 00:31:08 +0200

    New workflow

--- .github/release_message.sh
+++ .github/release_message.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-previous_tag=$(git tag --sort=-creatordate | sed -n 2p)
+previous_tag=$(git tag --sort=-creatordate | sed -n 2p) # This requires that at least one tag have been created previously to work.
 git shortlog "${previous_tag}.." | sed 's/^./    &/'
